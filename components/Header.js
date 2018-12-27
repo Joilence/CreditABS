e330d379832e9ce98d49d80b3ed0089c44902eb8
Joilence <cjyk84719@gmail.com>
2018-12-27 10:26:07 +0800

    show account and balance

---
+++ components/Header.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+
+import { Link } from '../routes';
+
+const styles = {
+  wrapper: {
+    margin: '0 auto',
+    width: '80%',
+    maxWidth: '1200px',
+    display: 'flex',
+  },
+  brand: {
+    borderRight: '2px solid #CCCCCC',
+    paddingRight: '1em',
+    marginRight: '1em',
+  },
+  toolbar: {
+    padding: 0,
+    flex: 1,
+  },
+  flexContainer: {
+    flex: 1,
+    display: 'flex',
+  },
+  anchor: {
+    display: 'block',
+    textDecoration: 'none',
+    marginRight: '16px',
+  },
+};
+
+class Header extends React.Component {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <AppBar position="static" color="default">
+        <div className={classes.wrapper}>
+          <Toolbar className={classes.toolbar}>
+            <Typography variant="title" color="inherit" className={classes.brand}>
+              Credit ABS
+            </Typography>
+            <p className={classes.flexContainer}>
+              <a href="/" className={classes.anchor}>
+                <Typography variant="title" color="inherit">
+                  Security List
+                </Typography>
+              </a>
+            </p>
+            <Link route="/projects/create">
+              <Button variant="raised" color="primary">
+                Create Security
+              </Button>
+            </Link>
+          </Toolbar>
+        </div>
+      </AppBar>
+    );
+  }
+}
+
+export default withStyles(styles)(Header);
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
+
+import web3 from '../libs/web3';
 import withRoot from '../libs/withRoot';
+import Layout from '../components/Layout';
 
 class Index extends React.Component {
-  render() {
-    return <Button variant="raised" color="primary">CreditABSCreditABS</Button>;
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            accounts: [],
+        };
+    }
+
+    async componentDidMount() {
+        const accounts = await web3.eth.getAccounts();
+        const balances = await Promise.all(accounts.map(x => web3.eth.getBalance(x)));
+        console.log({ accounts, balances });
+
+        this.setState({ accounts: accounts.map((x, i) => ({ account: x, balance: balances[i] })) });
+    }
+
+    render() {
+
+        const {accounts} = this.state;
+
+        return (
+            <Layout>
+                    <ul>
+                      {accounts.map(x => (
+                        <li key={x.account}>
+                          {x.account} => {web3.utils.fromWei(x.balance, 'ether')} ETH
+                        </li>
+                      ))}
+                    </ul>
+            </Layout>
+        );
+    }
 }
 
 export default withRoot(Index);
