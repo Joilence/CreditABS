130f2d2e541fb4d35fddb11cdfe54a42ac9058d1
Joilence <cjyk84719@gmail.com>
2018-12-28 18:29:00 +0800

    add info to grid

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,53 @@
 import React from 'react';
-import { Grid, Button, Typography, Card, CardContent, CardActions } from '@material-ui/core';
+import { Grid, Button, Typography, Card, CardContent, CardActions, LinearProgress } from '@material-ui/core';
 
 import { Link } from '../routes';
 import web3 from '../libs/web3';
+import CreditABS from '../libs/creditABS'
 import SecurityManager from '../libs/securityManager';
 import withRoot from '../libs/withRoot';
 import Layout from '../components/Layout';
+import InfoBlock from '../components/InfoBlock';
 
 class Index extends React.Component {
     static async getInitialProps({ req }) {
-        const ABSs = await SecurityManager.methods.getABSList().call();
+        const addressList = await SecurityManager.methods.getABSList().call();
+        const summaryList = await Promise.all(
+          addressList.map(address =>
+            CreditABS(address)
+              .methods.getSummary()
+              .call()
+          )
+        );
+        console.log({ summaryList });
+        const ABSs = addressList.map((address, i) => {
+            const [
+                name,
+                issuer,  
+                financingGoal,
+                fundReceived,
+                description,
+                numOfTokenholders,
+                contractBalance,
+                numOfPayments
+            ] = Object.values(
+                summaryList[i]
+            );
+    
+            return {
+                name,
+                issuer,  
+                financingGoal,
+                fundReceived,
+                description,
+                numOfTokenholders,
+                contractBalance,
+                numOfPayments
+            };
+        });
+    
+        console.log(ABSs);
+
         return { ABSs };
     }
 
@@ -26,22 +64,31 @@ class Index extends React.Component {
     }
 
     renderABS(abs) {
+
+        const progress = abs.contractBalance / abs.financingGoal * 100;
+
         return (
-            <Grid item md={4} key={abs}>
+            <Grid item md={6} key={abs.address}>
                 <Card>
                     <CardContent>
                         <Typography gutterBottom variant="headline" component="h2">
-                            {abs}
+                            {abs.name}
                         </Typography>
-                        <Typography component="p">{abs}</Typography>
+                        <LinearProgress style={{ margin: '10px 0' }} color="primary" variant="determinate" value={progress} />
+                        <Grid container spacing={16}>
+                            <InfoBlock title={`${web3.utils.fromWei(abs.financingGoal, 'ether')} ETH`} description="Financing Goal" />
+                            <InfoBlock title={`${web3.utils.fromWei(abs.fundReceived, 'ether')} ETH`} description="Fund Received" />
+                            <InfoBlock title={`${abs.numOfTokenholders} Tokenholders`} description="Tokenholders" />
+                            <InfoBlock title={`${web3.utils.fromWei(abs.contractBalance, 'ether')} ETH`} description="Contract Balance" />
+                        </Grid>
                     </CardContent>
                     <CardActions>
-                        <Link route={`/projects/${abs}`}>
+                        <Link route={`/projects/${abs.address}`}>
                             <Button size="small" color="primary">
                                 Purchase
                             </Button>
                         </Link>
-                        <Link route={`/projects/${abs}`}>
+                        <Link route={`/projects/${abs.address}`}>
                             <Button size="small" color="secondary">
                                 Details
                             </Button>
